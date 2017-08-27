df040bb4b61a34fcb8ded41b086c3ab6bf7bcc45
Prince-Polka <andre.hallqvist@gmail.com>
2017-08-27 20:30:23 +0200

    Update hsb2rgb.glsl

--- hsb2rgb.glsl
+++ hsb2rgb.glsl
@@ -17,10 +17,6 @@ vec3 hue(float H){
     return clamp(vec3(abs(H-3.)-1.0,2.-abs(H-2.),2.-abs(H-4.)),zero,one);
 }
 
-float natan2(vec2 a){
-    return 1.0-mod(0.25+(atan(a.x,a.y)+PI)/(PI*2.0),1.0);
-}
-
 vec3 hsb2rgb(vec3 hsb){
     vec3 rgb=vec3(mix(vec3(1.0),hue(hsb.r),hsb.g));
          rgb=vec3(mix(vec3(0.0),rgb,hsb.b));
@@ -33,14 +29,16 @@ vec3 rgb2hsb(vec3 c){
     vec3 foo =((c-c.gbr)/(high-low)+_024)*vec3(c.b==high,c.r==high,c.g==high);
     return clamp(vec3(mod(4.0+dot(foo,one),6.0)/6.0,(high-low)/high,high),zero,one);
 }
-
+// uncomment stuff to test the functions
+//float natan2(vec2 a){return 1.0-mod(0.25+(atan(a.x,a.y)+PI)/(PI*2.0),1.0);}
 void main() {
-    vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    vec3 test = vec3(1.0,1.0,1.0);
+    //vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    //vec3 test = vec3(1.0,1.0,1.0);
     //test=rgb2hsb(test);
     //test=hsb2rgb(test);
+    //gl_FragColor = vec4(test,1.0);
     //gl_FragColor = vec4(hsb2rgb(vec3(mod(u_time,1.0),st.x,1.0-st.y)),1.0);
     //gl_FragColor = vec4(hsb2rgb(vec3(st.x,1.0,1.0)),1.0);
     //gl_FragColor = vec4(hsb2rgb(vec3(mod(u_time,1.0),st.x,1.0)),1.0);
-    gl_FragColor = vec4(hsb2rgb(vec3(natan2(st.xy-.5),length(st.xy-.5)*2.0,mod(u_time,1.0))),1.0);
+    //gl_FragColor = vec4(hsb2rgb(vec3(natan2(st.xy-.5),length(st.xy-.5)*2.0,mod(u_time,1.0))),1.0);
 }
