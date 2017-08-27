df2f9f27fdfe5219760b94e355bc2617afb2a0c8
Prince-Polka <andre.hallqvist@gmail.com>
2017-08-27 07:09:38 +0200

    Update hsb2rgb.glsl

--- hsb2rgb.glsl
+++ hsb2rgb.glsl
@@ -22,13 +22,27 @@ vec3 hsb2rgb(vec3 hsb){
          rgb=vec3(mix(vec3(0.0),rgb,hsb.b));
     return clamp(rgb,vec3(0.0),vec3(1.0));
 }
-
 vec3 hsl2rgb(vec3 hsb){
-    vec3 rgb=vec3(mix(vec3(hsb.b),hue(hsb.r),1.0-abs(hsb.b-0.5)*2.0));
-    rgb=vec3(mix(vec3(hsb.b),rgb,hsb.g));
-    return rgb;
+    vec3 rgb=vec3(mix(vec3(hsb.b),hue(hsb.r),hsb.g));
+         rgb=vec3(mix(vec3(hsb.b),rgb,1.0-abs(hsb.b-0.5)*2.0));
+    return clamp(rgb,vec3(0.0),vec3(1.0));
 }
 
+vec3 rgb2hsl(vec3 c){
+    float low=min(min(c.r,c.g),c.b);
+    float high=max(max(c.r,c.g),c.b);
+    float D=1.0/(high-low);
+    float h,s,l;
+    h=float(c.r==high)*(c.g-c.b)*D;
+    h+=float(c.g==high)*((c.b-c.r)*D+2.0);
+    h+=float(c.b==high)*((c.r-c.g)*D+4.0);
+    h=mod(h,6.0)/6.0;
+    l=(high+low)*0.5;
+    s=(high-low);
+    s/=1.0-abs(high+low-1.0);
+    return vec3(h,s,l);
+}
+   
 vec3 rgb2hsb(vec3 c){
     float low=min(min(c.r,c.g),c.b);
     float high=max(max(c.r,c.g),c.b);
@@ -41,9 +55,9 @@ vec3 rgb2hsb(vec3 c){
 }
 void main() {
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    vec3 test = vec3(0.167,0.149,0.200);
+    vec3 test = vec3(0.226,0.355,0.229);
     test=rgb2hsb(test);
     test=hsb2rgb(test);
     gl_FragColor =vec4(test,1.0);
-    //gl_FragColor = vec4(hsb2rgb(vec3(mod(u_time,1.0),st.x,1.0-st.y)),1.0);
+    //gl_FragColor = vec4(hsl2rgb(vec3(mod(u_time,1.0),st.x,1.0-st.y)),1.0);
 }
