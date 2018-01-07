5412a998847af59cbcedc82aec5c94ac70d87989
Prince-Polka <andre.hallqvist@gmail.com>
2018-01-07 06:22:23 +0100

    Create C(gcc)implementation.c

---
+++ C(gcc)implementation.c
@@ -0,0 +1,44 @@
+/* not clean but should be branchless */
+
+#define ABS(x)(x*((x>0)*2-1))
+
+/*int clamp(int x,int min, int max){
+int temp = min*(x<min) + max*(x>max);
+return temp + x * !temp;
+}*/
+#define clamp(val,min,max)(val<min?min:val>max?max:val)
+
+/* shuffle components to meet your needs */
+typedef union color{
+struct {char blue,green,red,alpha;};
+signed int i;
+unsigned int u;
+}color;
+
+/* fixed range 360,100,100 -> 255 */
+color hsb2rgb(int H, int S, int V) {
+  int R, G, B;
+  H *= 17;
+  R = -1020+abs(H-3060);
+  G =  2040-abs(H-2040);
+  B =  2040-abs(H-4080);
+  R = clamp(R, 0, 1020)-1020;
+  G = clamp(G, 0, 1020)-1020;
+  B = clamp(B, 0, 1020)-1020;
+
+  R = ((R*S+102000)*V)/40000;
+  G = ((G*S+102000)*V)/40000;
+  B = ((B*S+102000)*V)/40000;
+
+  /* shuffle components to meet your needs */
+  color ret = {B,G,R,255};
+  return ret;
+}
+
+int main(void){
+color mycolor = hsb2rgb(200,80,80);
+printf("0x%x\n",mycolor);
+mycolor = hsb2rgb(120,100,100);
+printf("0x%x\n",mycolor);
+return 0;
+}
